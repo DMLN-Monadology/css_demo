e4045b6623b98d36eeb33f2b516aa0d7774cab61
Duc (Stian) M. L. Nguyen <duc.m.nguyen@yale.edu>
2016-11-30 23:29:11 -0800

    flip cards completed

---
+++ flip.js
@@ -0,0 +1,11 @@
+$('.AlexanderNevsky').click( () => {
+  $('.AlexanderNevsky').toggleClass('FlipRight');
+});
+
+$('.PyotrVelikiy').click( () => {
+  $('.PyotrVelikiy').toggleClass('FlipLeft');
+});
+
+$('.YuriGagarin').click( () => {
+  $('.YuriGagarin').toggleClass('FlipRight');
+});
